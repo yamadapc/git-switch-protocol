93a11a62d7d1a2d22e3c8f99bb01223b69f41793
yamadapc <tacla.yamada@gmail.com>
2014-09-02 21:55:35 -0300

    Initial working commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+git-switch-protocol
+===================
+A helper to easily switch a git repositories remotes from `ssh` to `https` and
+back. Useful for places where `ssh` is blocked (such as some mermaid
+coffee-shops in São Paulo, as of recently)
+
+## Usage
+```bash
+$ git-switch-protocol -p https
+Switching all remotes to `https`...
+  > git remote -v
+Switching remote `origin` from `ssh` to `https`...
+  > git remote set-url origin https://github.com/yamadapc/pyjamas.git
+Remote `bitbucket` is already using `https`. Skipping...
+```
+
+## License
+This code is licensed under the GPLv3 license. See the [LICENSE](/LICENSE) file
+for more information.
---
+++ source/app.d
@@ -0,0 +1,160 @@
+// Licensed under the GPLv3 license. See LICENSE for more information.
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.getopt;
+import std.process;
+import std.regex;
+import std.string;
+
+import colorize;
+
+static immutable string usage = "Usage: git-switch-protocol -p <protocol>";
+
+version(unittest) void main() {}
+else
+int main(string[] args)
+{
+  if(args.length == 1)
+  {
+    usage.color("yellow").cwriteln;
+    return 1;
+  }
+
+  try
+  {
+    bool help;
+    string targetRemote;
+    protocol targetProtocol;
+
+    getopt(
+      args,
+      "protocol|p", &targetProtocol,
+      "help|h", &help
+    );
+
+    if(help)
+    {
+      usage.color("yellow").cwriteln;
+      return 1;
+    }
+
+    "Switching all remotes to `%s`..."
+      .format(targetProtocol.to!string)
+      .color("light_blue")
+      .cwriteln;
+
+    foreach(remote; readRemotes)
+      remote.switchToProtocol(targetProtocol);
+    return 0;
+  }
+  catch(Exception err)
+  {
+    "ERROR: %s".format(err.msg).color("red").cwriteln;
+    return 1;
+  }
+}
+
+enum protocol { ssh, https }
+
+struct Remote
+{
+  protocol type;
+  string name;
+  string host;
+  string path;
+
+  void switchToProtocol(protocol targetProtocol)
+  {
+    if(type == targetProtocol)
+    {
+      "Remote `%s` is already using `%s`. Skipping...".color("yellow")
+        .cwritefln(name, targetProtocol.to!string);
+      return;
+    }
+
+    cwritefln(
+      "Switching remote `%s` from `%s` to `%s`...".color("light_blue"),
+      name,
+      type.to!string,
+      targetProtocol.to!string
+    );
+
+    auto cmdResult = git("remote", [
+      "set-url",
+      name,
+      targetProtocol == protocol.https ? httpsUrl : sshUrl
+    ]);
+
+    auto exitCode = cmdResult.status;
+    if(exitCode != 0)
+    {
+      throw new Exception("Failed with code " ~ exitCode.to!string);
+    }
+  }
+
+  @property string sshUrl()
+  {
+    return "git@%s:%s".format(host, path);
+  }
+
+  @property string httpsUrl()
+  {
+    return "https://%s/%s".format(host, path);
+  }
+
+  unittest
+  {
+    auto r = Remote(protocol.https, "origin", "github.com", "yamadapc/pyjamas");
+    assert(r.sshUrl == "git@github.com:yamadapc/pyjamas");
+    assert(r.httpsUrl == "https://github.com/yamadapc/pyjamas");
+  }
+}
+
+Remote toRemote(in string descriptor)
+{
+  static auto rgx = ctRegex!(
+    `(?P<name>[^\s]+)\s`
+    `(?P<prefix>.+(@|(://)))`
+    `(?P<host>[^:/]+)(:|/)`
+    `(?P<path>[^\s]+)`
+  );
+  auto cs = descriptor.match(rgx).captures;
+
+  return Remote(
+    cs["prefix"] == "https://" ? protocol.https : protocol.ssh,
+    cs["name"],
+    cs["host"],
+    cs["path"]
+  );
+}
+
+unittest
+{
+  auto remote = toRemote(
+    "origin\tgit@github.com:yamadapc/git-switch-protocol.git (fetch)"
+  );
+
+  assert(remote.type == protocol.ssh);
+  assert(remote.name == "origin");
+  assert(remote.host == "github.com");
+  assert(remote.path == "yamadapc/git-switch-protocol.git");
+}
+
+auto readRemotes()
+{
+  auto result = git("remote", ["-v"]);
+  if(result.status != 0) throw new Exception("Coundn't fetch remotes");
+
+  return result.output
+    .split("\n")[0..$-1]
+    .map!(toRemote)
+    .uniq;
+}
+
+auto git(in string gitcmd, in string[] args = [])
+{
+  auto cmd = ["git", gitcmd] ~ args;
+  ("  >" ~ cmd).join(" ").color("light_green").cwriteln;
+  return execute(cmd);
+}
